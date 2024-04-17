df5f3ab9a3ab043bb081bf90115a4151022a72e8
gimhantharuke456 <gimhantharuke456@gmail.com>
2024-04-17 22:17:09 +0530

    test commit

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/trains", trainRoutes);
 const PORT = process.env.PORT || 4001;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running in port ${PORT}`);
 });
 
 module.exports = app;
